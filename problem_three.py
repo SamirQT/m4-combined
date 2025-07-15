c931c610da8fbf4ca2192315a485f49cd8fd14e1
SamirQT <143544211+SamirQT@users.noreply.github.com>
2025-07-15 22:03:32 +0300

    Create problem_three.py

---
+++ problem_three.py
@@ -0,0 +1,6 @@
+def process_list(nums: list[int], k: int) -> list[int]:
+    """
+    1. Circularly shift `nums` right by `k` positions.
+    2. Compute its average.
+    3. Return a new list of values ≥ average.
+    """
