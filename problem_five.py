bba1ca8f2f5914154157d4c0d00099ad8bbd1ad0
SamirQT <143544211+SamirQT@users.noreply.github.com>
2025-07-15 22:04:15 +0300

    Create problem_five.py

---
+++ problem_five.py
@@ -0,0 +1,5 @@
+def longest_increasing_run(nums: list[int]) -> tuple[int, int]:
+    """
+    Find the longest strictly increasing contiguous subsequence (“run”)
+    in `nums`. Return (length, start_index). If `nums` is empty, return (0, 0).
+    """
