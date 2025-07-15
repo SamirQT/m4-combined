5c20d26b192a5c256f3e60235042f121a75b224c
SamirQT <143544211+SamirQT@users.noreply.github.com>
2025-07-15 22:03:05 +0300

    Create problem_two.py

---
+++ problem_two.py
@@ -0,0 +1,5 @@
+def interleave(a, b):
+    """
+    Given two sequences `a` and `b` of the same length (both `str` or both `list`),
+    return a new sequence of the same type by alternating their elements.
+    """
